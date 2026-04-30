c20011be287222d8da744b3e332c0bc12a38d564
tao.zang <tower1229@foxmail.com>
2026-04-30 10:49:39 +0800

    fix: update effect dependency to trigger refresh on location change

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -67,7 +67,7 @@ export default function App() {
 
   useEffect(() => {
     refresh();
-  }, []);
+  }, [location.pathname]);
 
   const statusLabel = useMemo(() => {
     if (state === "loading") {
